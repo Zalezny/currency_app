dea97bb4d7db9741d66272e3a8212b04c5c51005
Zalezny <zaleznypl@gmail.com>
2023-04-25 08:47:08 +0200

    created top row

---
+++ lib/custom_widgets/top_row.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class TopRow extends StatelessWidget {
+  final String text;
+  const TopRow({super.key, required this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
+        children: [
+          GestureDetector(
+            behavior: HitTestBehavior.translucent,
+            onTap: () {
+              Navigator.of(context).pop();
+            },
+            child: Padding(
+              padding: const EdgeInsets.only(
+                top: 8.0,
+                bottom: 8.0,
+                left: 8.0,
+                right: 16.0,
+              ),
+              child: Icon(
+                Icons.arrow_back_ios_new_rounded,
+                color: Theme.of(context).primaryColor,
+              ),
+            ),
+          ),
+          Expanded(
+              child: Text(
+            text,
+            style: const TextStyle(
+                color: Colors.white,
+                fontFamily: 'Roboto',
+                fontSize: 24,
+                fontWeight: FontWeight.w700),
+            textAlign: TextAlign.start,
+          )),
+        ],
+      ),
+    );
+  }
+}
--- lib/pages/currency_page/currency_page.dart
+++ lib/pages/currency_page/currency_page.dart
@@ -1,3 +1,4 @@
+import 'package:currency_app/custom_widgets/top_row.dart';
 import 'package:currency_app/utils/currency_code.dart';
 import 'package:currency_app/pages/currency_page/bloc/currency_bloc.dart';
 import 'package:currency_app/pages/currency_page/widgets/currency_chart.dart';
@@ -20,25 +21,21 @@ class CurrencyPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
         child: Scaffold(
-            appBar: AppBar(
-              title: const Text("Details"),
-            ),
             body: BlocProvider(
-              create: ((context) =>
-                  CurrencyBloc(currencyConnection, code, valueOfDays)
-                    ..add(SendCurrencyEvent())),
-              child: BlocBuilder<CurrencyBloc, CurrencyState>(
-                builder: (context, state) {
-                  if (state is CurrencySuccessState) {
-                    return _buildBody(state.model, context);
-                  } else if (state is CurrencyFailState) {
-                    return _buildBody(null, context);
-                  } else {
-                    return _buildBody(null, context);
-                  }
-                },
-              ),
-            )));
+      create: ((context) => CurrencyBloc(currencyConnection, code, valueOfDays)
+        ..add(SendCurrencyEvent())),
+      child: BlocBuilder<CurrencyBloc, CurrencyState>(
+        builder: (context, state) {
+          if (state is CurrencySuccessState) {
+            return _buildBody(state.model, context);
+          } else if (state is CurrencyFailState) {
+            return _buildBody(null, context);
+          } else {
+            return _buildBody(null, context);
+          }
+        },
+      ),
+    )));
   }
 
   Widget _buildBody(CurrencyDto? model, BuildContext context) {
@@ -47,6 +44,7 @@ class CurrencyPage extends StatelessWidget {
 
     return Column(
       children: [
+        TopRow(text: "Details ${code.currencyCode}"),
         SizedBox(height: mediaQuery.height * 0.03),
         Card(
           shape: RoundedRectangleBorder(
